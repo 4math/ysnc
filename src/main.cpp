8357cb4d392c95a371fd5b2527cf2de5c7ac5a29
cyoq <di9akoz@gmail.com>
2021-09-17 22:55:27 +0300

    Added entrypoint for the program

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <tokenizer.h>
+#include <suffix_tree.h>
+
+int main() {
+    Tokenizer::display();
+    SuffixTree::display();
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
