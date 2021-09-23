e0e86d5c7732ece6ea58db50940c6bb8c5d77654
Kirill <kirilltrofimov@inbox.lv>
2021-09-23 17:54:10 +0300

    started coding tokenizer

--- src/analyzer.h
+++ src/analyzer.h
@@ -1,9 +1,16 @@
 #pragma once
 
+#include "tokenizer.h"
+
 class Analyzer {
 public: 
 	Analyzer(const std::string& source, const std::string& target) {
+		Tokenizer tokenizer(source);
+		const auto& tokenizedSource = tokenizer.result();
+		for (auto& token : tokenizedSource)
+			std::cout << token.getValue() << ":" << token.getId() << " ";
 
+		std::cout << "\n";
 	}
 
 	float result() {
--- src/tokenizer.h
+++ src/tokenizer.h
@@ -1,11 +1,117 @@
 #pragma once
 
+#include <map>
+#include <string>
+
 class Tokenizer {
 public:
-	Tokenizer() {
+	class Token {
+	public:
+		Token(int position_, int id_) : position( position_ ), id( id_ ) {
+			value = reverseTokensMap[id];
+		}
 
+		Token(int position_, const std::string& value_) : position(position_), value( value_ ) {
+			id = tokensMap[value];
+		}
+
+		int getId() const {
+			return id;
+		}
+
+		const std::string& getValue() const {
+			return value;
+		}
+
+	private:
+		int position;
+		int id;
+		std::string value;
+	};
+
+	Tokenizer(const std::string& inputString_) : inputString(inputString_) {}
+
+	std::vector<Token>& result() {
+		int pos = 0;
+		Token t(pos++, "package");
+		Token t1(pos++, "const");
+		Token t2(pos++, 40);
+
+		tokenVector = { t, t1, t2 };
+		return tokenVector;
+	}
+
+	typedef std::map<std::string, int> tMap;
+	typedef std::map<int, std::string> rtMap;
+
+	static tMap tokensMap;
+	static rtMap reverseTokensMap;
+
+	static rtMap initReverseTokensMap() {
+		rtMap map;
+
+		for (auto it = tokensMap.begin(); it != tokensMap.end(); it++)
+			map[it->second] = it->first;
+
+		return map;
 	}
 
 private:
-
+	const std::string& inputString;
+	std::vector<Token> tokenVector;
 };
+
+Tokenizer::tMap Tokenizer::tokensMap = {
+	{"abstract", 1},
+	{"boolean", 2},
+	{"byte", 3},
+	{"break", 4},
+	{"class", 5},
+	{"case", 6},
+	{"catch", 7},
+	{"char", 8},
+	{"continue", 9},
+	{"default", 10},
+	{"do", 11},
+	{"double", 12},
+	{"else", 13},
+	{"extends", 14},
+	{"final", 15},
+	{"finally", 16},
+	{"float", 17},
+	{"for", 18},
+	{"if", 19},
+	{"implements", 20},
+	{"import", 21},
+	{"instanceof", 22},
+	{"int", 23},
+	{"interface", 24},
+	{"long", 25},
+	{"native", 26},
+	{"new", 27},
+	{"package", 28},
+	{"private", 29},
+	{"protected", 30},
+	{"public", 31},
+	{"return", 32},
+	{"short", 33},
+	{"static", 34},
+	{"super", 35},
+	{"switch", 36},
+	{"synchronized", 37},
+	{"this", 38},
+	{"throw", 39},
+	{"throws", 40},
+	{"transient", 41},
+	{"try", 42},
+	{"void", 43},
+	{"volatile", 44},
+	{"while", 45},
+	{"assert", 46},
+	{"const", 47},
+	{"enum", 48},
+	{"goto", 49},
+	{"strictfp", 50}
+};
+
+Tokenizer::rtMap Tokenizer::reverseTokensMap = Tokenizer::initReverseTokensMap();
